7c5c444f00e9437b315da12c691a0ec442145e69
malik <mrabdmalik@gmail.com>
2022-10-30 15:39:06 +0800

    http POST & PUT method

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,14 +15,50 @@ export class HomePage {
   }
 
   getData() {
-    this.http.get<any>('https://jsonplaceholder.typicode.com/todos/50').subscribe(
-      (data) => {
-        console.log(data);
-        this.data = data; //put data into this.data array
-      },
-      (e) => {
-        console.log(e);
-      }
-    );
+    this.http
+      .get<any>('https://jsonplaceholder.typicode.com/todos/50')
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.data = data; //put data into this.data array
+        },
+        (e) => {
+          console.log(e);
+        }
+      );
+  }
+
+  postData() {
+    // this.http
+    //   .post<any>('https://jsonplaceholder.typicode.com/create_todos', {
+    //     title: 'ABCD',
+    //     completed: true,
+    //     userId: 40,
+    //   })
+    //   .subscribe(
+    //     (data) => {
+    //       console.log(data);
+    //       this.data = data;
+    //     },
+    //     (e) => {
+    //       console.log(e);
+    //     }
+    //   );
+
+    this.http
+      .put<any>('https://jsonplaceholder.typicode.com/create_todos', {
+        title: 'ABCD',
+        completed: true,
+        userId: 40,
+      })
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.data = data;
+        },
+        (e) => {
+          console.log(e);
+        }
+      );
   }
 }

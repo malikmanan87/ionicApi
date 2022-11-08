dd45e085557bd275d907b9f0b6cc259405d8821d
malik <mrabdmalik@gmail.com>
2022-11-08 11:59:05 +0800

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CallNumber } from "@awesome-cordova-plugins/call-number/ngx";
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    HttpClientModule,
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [CallNumber,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    // CallNumber,
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/contacts/contacts.page.ts
+++ src/app/home/contacts/contacts.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
 import { Contacts } from '@capacitor-community/contacts';
 import { isPlatform } from '@ionic/angular';
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.page.html',
   styleUrls: ['./contacts.page.scss'],
 })
 export class ContactsPage implements OnInit {
+
   contacts: any[] = [];
 
-  constructor() {}
+  constructor(private callNumber: CallNumber) { }
 
   ngOnInit() {
     this.getContacts();
@@ -17,18 +20,34 @@ export class ContactsPage implements OnInit {
 
   async getContacts() {
     try {
-      if (isPlatform('android')) {
-        const permissions = await Contacts.getPermissions();
-        if (!permissions.granted) return;
+      if(isPlatform('android')) {
+        const permission = await Contacts.getPermissions();
+        if(!permission.granted) return;
       }
       const result = await Contacts.getContacts();
+      // (result.contacts).slice(0, 10).map((item, i) => {
+      //   this.contacts.push(item);
+      // });
       this.contacts = result.contacts;
       console.log(this.contacts);
       // for (const contact of result.contacts) {
-      //   console.log(contact);
+      //     console.log(contact);
       // }
-    } catch (e) {
+    } catch(e) {
       console.log(e);
+    }
+  }
+  
+  call(contact) {
+    console.log(contact);
+    let phoneNumber = contact.phoneNumbers[0];
+    if(phoneNumber) {
+      if(isPlatform('ios')) {
+        phoneNumber = phoneNumber.number;
+      }
+      this.callNumber.callNumber(phoneNumber, true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
     }
   }
 }
